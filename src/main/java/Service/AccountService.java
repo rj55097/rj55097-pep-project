ed21ee2ac7d3b1439f3f3279f4bb241286a173f1
Rose Jewel <roseajewel@gmail.com>
2025-01-03 09:57:03 +0000

    added necessary imports and constructors

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -1,5 +1,21 @@
 package Service;
 
+import Model.Account;
+import DAO.AccountDAO;
+
+import java.util.List;
+
+
 public class AccountService {
+    public AccountDAO accountDAO;
+
+    // no-args constructor
+    public AccountService(){
+        accountDAO = new AccountDAO();
+    }
     
+    // constructor for when accountDAO is provided
+    public AccountService(AccountDAO accountDAO){
+        this.accountDAO = accountDAO;
+    }
 }
--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -1,5 +1,21 @@
 package Service;
 
+import Model.Message;
+import DAO.MessageDAO;
+
+import java.util.List;
+
 public class MessageService {
+    public MessageDAO messageDAO;
+
+    // no-args constructor
+    public MessageService(){
+        messageDAO = new MessageDAO();
+    }
+    
+    // constructor for when messageDAO is provided
+    public MessageService(MessageDAO messageDAO){
+        this.messageDAO = messageDAO;
+    }
     
 }
